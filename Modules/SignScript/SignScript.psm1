fe1a8147fa3595c13edc5d0b318cbe4383d897d6
TheOneTrueTrench <trenchie@trenchie.us>
2018-12-10 18:38:15 -0500

    Added support for username and machine as the keyname.

--- Modules/SignScript/SignScript.psm1
+++ Modules/SignScript/SignScript.psm1
@@ -1,38 +1,38 @@
-function SignScript {                  
-    $barnoldCert = (dir cert:LocalMachine\My\ -CodeSigningCert -DnsName "trenchie@trenchie.us")[0]          
-    $loc = @()                                
-    foreach ($arg in ($args + $input)) {                                                                                                   
-        $startFolder = Get-Location                                                                                                   
-        $location = (Get-Item -Path $arg)                                                                                             
-        if ($location.Mode.StartsWith("d")) {  
+function SignScript {
+    $barnoldCert = (dir cert:LocalMachine\My\ -CodeSigningCert -DnsName ($env:USERNAME + "@" + $env:COMPUTERNAME))[0]
+    $loc = @()
+    foreach ($arg in ($args + $input)) {
+        $startFolder = Get-Location
+        $location = (Get-Item -Path $arg)
+        if ($location.Mode.StartsWith("d")) {
             $loc += $arg
-            Set-Location $location                                                                                                
-            $childFiles = $location.GetFiles()                                                                                    
-            SignScript @childFiles                                                                                                
-            $childDirs = $location.GetDirectories()                                                                               
-            SignScript @childDirs                                                                                                 
+            Set-Location $location
+            $childFiles = $location.GetFiles()
+            SignScript @childFiles
+            $childDirs = $location.GetDirectories()
+            SignScript @childDirs
             Set-Location $startFolder
             if ($loc.Length > 1)
             {
                 $loc = $loc[0..($loc.Length-2)];
-            }   
+            }
             else
             {
                 $loc = @()
-            }                                                                                          
-        }                                                                                                                             
-        else {                                                                                                                                                
+            }
+        }
+        else {
             Write-Host("Signing ") -NoNewline
             Write-Host($arg) -NoNewline
             Write-Host([System.String]::Join("", $loc)) -NoNewline
-            $result =Set-AuthenticodeSignature -FilePath $arg -Certificate $barnoldCert -TimestampServer http://timestamp.comodoca.com/authenticode                        
-            Write-Host("... ") -NoNewline                                                                                         
-            switch ($result.Status) {                                                                                             
-                "Valid" { Write-Host $result.StatusMessage -ForegroundColor Black -BackgroundColor Green }                    
-                Default { Write-Host $result.StatusMessage -ForegroundColor Black -BackgroundColor Red }                      
-            }                                                                                                                     
-        }                                                                                                                                  
-    }                                                                                                                                     
-}     
+            $result =Set-AuthenticodeSignature -FilePath $arg -Certificate $barnoldCert -TimestampServer http://timestamp.comodoca.com/authenticode
+            Write-Host("... ") -NoNewline
+            switch ($result.Status) {
+                "Valid" { Write-Host $result.StatusMessage -ForegroundColor Black -BackgroundColor Green }
+                Default { Write-Host $result.StatusMessage -ForegroundColor Black -BackgroundColor Red }
+            }
+        }
+    }
+}
 
 Export-ModuleMember -Function SignScript
--- createCert.ps1
+++ createCert.ps1
@@ -1,6 +1,6 @@
-if (((ls Cert:\LocalMachine\My | where { $_.Subject -eq "CN=trenchie@trenchie.us" } ).Count -lt 1) -And (($PSVersionTable.PSVersion).Major -lt 6)) 
+if (((ls Cert:\LocalMachine\My | where { $_.Subject -eq "CN=" + $env:USERNAME + "@" + $env:COMPUTERNAME } ).Count -lt 1) -And (($PSVersionTable.PSVersion).Major -lt 6))
 {
-        $cert = New-SelfSignedCertificate -DnsName 'trenchie@trenchie.us' -CertStoreLocation Cert:\LocalMachine\My\ -Type Codesigning
+        $cert = New-SelfSignedCertificate -DnsName ($env:USERNAME + "@" + $env:COMPUTERNAME) -CertStoreLocation Cert:\LocalMachine\My\ -Type Codesigning
         Export-Certificate -Cert $cert -FilePath signing.cer
         Import-Certificate -FilePath .\signing.cer -CertStoreLocation Cert:\LocalMachine\Root
         Import-Certificate -FilePath .\signing.cer -CertStoreLocation Cert:\LocalMachine\TrustedPublisher\
